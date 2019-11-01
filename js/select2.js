103bed6a0c18aa62de21d17f03c6d7ebac250b17
Daniel Vena <deaconvena+git@gmail.com>
2019-11-01 18:18:22 -0400

    customize select2

--- js/select2.js
+++ js/select2.js
@@ -1,33 +1,40 @@
 (function($, window, undefined) {
 	$(function() {
 
-		$('select.populate_markets').select2({
-			allowClear: true,
-			multiple: true,
-			closeOnSelect: false,
-			ajax: {
-				url: 'https://player.westwoodone.com/stations/stations.ashx',
-				dataType: 'json',
-				processResults: function(data) {
-					var markets = [];
-					data.forEach(function(station) {
-						var market = station.city + ', ' + station.state;
-						if ( ! markets.includes(market)) {
-							markets.push(market);
-						}
-					});
-					markets.sort();
-					return {
-						results: markets
-					};
+		var populate_markets = $('select.populate_markets');
+		populate_markets.each(function() {
+			var $select = $(this);
+			$select.select2({
+				allowClear: true,
+				multiple: true,
+				closeOnSelect: false,
+				dropdownParent: $select.parents('.wpcf7'),
+				width: '100%',
+				ajax: {
+					url: 'https://player.westwoodone.com/stations/stations.ashx',
+					dataType: 'json',
+					processResults: function(data) {
+						var markets = [];
+						data.forEach(function(station) {
+							var market = station.city + ', ' + station.state;
+							if ( ! markets.includes(market)) {
+								markets.push(market);
+							}
+						});
+						markets.sort();
+						return {
+							results: markets
+						};
+					}
 				}
-			}
+			});
 		});
 
 		$('.wpcf7 select:not(.populate_markets)').each(function() {
 			var $select = $(this);
 			$select.select2({
-				multiple: $select.attr('multiple') ? true : false
+				multiple: $select.attr('multiple') ? true : false,
+				width: '100%'
 			});
 		});
 
