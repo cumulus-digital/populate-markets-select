54551d9ac5f475c7a00289482dd76593fc200d62
Daniel Vena <deaconvena+git@gmail.com>
2019-11-01 14:57:53 -0400

    initial commit

---
+++ plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Populate Markets Select
+ * Plugin URI: https://github.com/cumulus-digital/populate-markets-select
+ * Description: With Caldera Forms and Popup Maker, allow a field with populate_markets class to fetch station markets from the station finder.
+ * Author: vena
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: cumulus-digital/populate-markets-select
+ */
+
+function populate_markets_select_init_scripts() {
+	wp_enqueue_script(
+		'populate_markets_select_init_scripts',
+		plugins_url( 'populate_markets.js', dirname( __FILE__) ),
+		array('jquery'), null, true
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'populate_markets_select_init_scripts' );
---
+++ populate_markets.js
@@ -0,0 +1,59 @@
+(function($, window, undefined) {
+	$(function() {
+
+		// Operations for all Caldera Autoselect fields
+		var all_caldera_autoselects = $('.ccselect2-container');
+		if (all_caldera_autoselects.length) {
+
+			// Close select2 when Popup Maker closes
+			$('.pum').on('pumBeforeClose', function() {
+				var $this = jQuery(this);
+				$this.find('.ccselect2-container ~ select').select2('close');
+			});
+
+			// Close all autoselects if clicking anywhere else
+			$(document).on('click', function(e) {
+				if (e.target.className.indexOf('ccselect2') < 0) {
+					all_caldera_autoselects.select2('close');
+				}
+			});
+
+		}
+
+		function getMarkets(callback) {
+			$.getJSON(
+				"https://player.westwoodone.com/stations/stations.ashx",
+				function( data ) {
+					var markets = [];
+					data.forEach(function(station) {
+						var market = station.city + ', ' + station.state;
+						if ( ! markets.includes(market)) {
+							markets.push(market);
+						}
+					});
+					markets.sort();
+					callback(markets);
+				}
+			);
+		}
+
+		// Operate on any Caldera Autoselect fields for populate_markets
+		var caldera_autoselects = $('.populate_markets .ccselect2-container ~ select');
+		if (caldera_autoselects.length) {
+
+			// Retrieve and fill markets
+			getMarkets(function(markets) {
+				var new_markets = []
+				markets.forEach(function(market) {
+					new_markets.push(new Option(market, market, false, false));
+				});
+				caldera_autoselects.each(function() {
+					var $select = $(this);
+					$select.append(new_markets).trigger('change');
+				});
+			})
+
+		}
+
+	});
+})(jQuery, window.self);
