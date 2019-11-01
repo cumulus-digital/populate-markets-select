a62cdaddb19552a96b05228e27636ef8ee4a1470
Daniel Vena <deaconvena+git@gmail.com>
2019-11-01 18:09:09 -0400

    fix select2 cdn link

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/cumulus-digital/populate-markets-select
  * Description: Auto-fill a select field with class name "populate_markets" from the station finder. Also adds styles and scripts for Contact Form 7.
  * Author: vena
- * Version: 1.0.25
+ * Version: 1.0.26
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  * GitHub Plugin URI: cumulus-digital/populate-markets-select
@@ -26,7 +26,7 @@ function populate_markets_select_init_scripts() {
 	if (is_plugin_active('contact-form-7/wp-contact-form-7.php')) {
 		wp_enqueue_script(
 			'populate_markets_select_select2_scripts',
-			'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.11/js/select2.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js',
 			array('jquery', 'populate_markets_select_init_scripts'), null, true
 		);
 		wp_enqueue_script(
@@ -36,7 +36,7 @@ function populate_markets_select_init_scripts() {
 		);
 		wp_register_style(
 			'populate_markets_select_select2_styles',
-			'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.11/css/select2.min.css',
+			'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.min.css',
 			false, null
 		);
 		wp_enqueue_style('populate_markets_select_select2_styles');
