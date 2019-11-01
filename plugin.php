cbf7ee0a5a82a68d144e82ee767b80051528f7a2
Daniel Vena <deaconvena+git@gmail.com>
2019-11-01 15:49:23 -0400

    need to enqueue style

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/cumulus-digital/populate-markets-select
  * Description: With Caldera Forms and Popup Maker, allow a field with populate_markets class to fetch station markets from the station finder.
  * Author: vena
- * Version: 1.0.8
+ * Version: 1.0.9
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  * GitHub Plugin URI: cumulus-digital/populate-markets-select
@@ -21,6 +21,7 @@ function populate_markets_select_init_scripts() {
 		plugins_url( 'css/populate_markets.css', __FILE__ ),
 		null, false, 'all'
 	);
+	wp_enqueue_style('populate_markets_select_init_styles');
 }
 
 add_action( 'wp_enqueue_scripts', 'populate_markets_select_init_scripts' );
