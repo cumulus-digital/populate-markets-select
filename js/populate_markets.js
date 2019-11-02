ba60bbb61cb389560a49c67a3050b0cd37836df5
Daniel Vena <deaconvena+git@gmail.com>
2019-11-02 11:57:59 -0400

    support form plugins that include select2 (fluent forms)

--- js/populate_markets.js
+++ js/populate_markets.js
@@ -1,6 +1,10 @@
 (function($, window, undefined) {
 	$(function() {
 
+		var isMobile = false;
+		var populate_markets = $('.populate_markets select, select.populate_markets');
+		var got_markets;
+
 		// Popup Maker forms need to be reset on open
 		$('.pum').on('pumBeforeOpen', function() {
 			var fluentform = $(this).find('.fluentform');
@@ -13,22 +17,75 @@
 			});
 		});
 
-		function handleSelects() {
-			var populate_markets = $('.populate_markets select, select.populate_markets');
-			if (populate_markets.length) {
-				$('body').append('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/css/select2.min.css"></link>');
-				$.getScript(
-					'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.10/js/select2.min.js',
-					function() {
-						populate_markets.each(function() {
-							var $select = $(this);
-							var original_options = $select.find('option').map(function() {
-								return $(this).text();
-							});
-							$select.on('select2:change', function() {
-								$select.trigger('change');
-							});
-							$select.select2({
+		// Don't use select2 on mobile
+		if (window.matchMedia && window.matchMedia('(pointer:none) or (pointer:coarse)').matches) {
+			isMobile = true;
+			if ( ! $.fn.select2 || ! $.fn.selectWoo) {
+				$(window).load(function() {
+					$('select').select2('disable');
+				});
+			}
+		}
+
+		function getMarkets(callback) {
+			if (got_markets) {
+				return got_markets;
+			}
+			$.getJSON(
+				'https://player.westwoodone.com/stations/stations.ashx',
+				function(data) {
+					var markets = [];
+					data.forEach(function(station) {
+						var market = station.city + ', ' + station.state;
+						if ( ! markets.includes(market)) {
+							markets.push(market);
+						}
+					});
+					markets.sort();
+					got_markets = markets;
+					if (callback) {
+						callback(got_markets);
+					}
+				}
+			);
+		}
+
+		if (populate_markets.length) {
+
+			// Load markets
+			getMarkets(function(markets) {
+				var market_opts = [];
+				markets.forEach(function(market) {
+					market_opts.push(
+						new Option(market, market, false, false)
+					);
+				});
+				var optgroup = $('<optgroup/>', { label: 'Markets' });
+				optgroup.append(market_opts);
+
+				populate_markets
+					.append(optgroup)
+					.trigger('change');
+
+				// Disable select2 on mobile
+				if ( ($.fn.select2 || $.fn.selectWoo) && isMobile ) {
+					populate_markets.select2('destroy');
+				}
+
+				// If desktop and select2 isn't loaded, load it.
+				if ( ( ! $.fn.select2 || ! $.fn.selectWoo) && ! isMobile) {
+					// Add Chosen stylesheet
+					$('body').append(
+						'<link/>', {
+							rel: 'preload',
+							href: 'https://cdn.jsdelivr.net/npm/select-woo@1.0.1/dist/css/selectWoo.min.css',
+							on: { load: 'this.rel="stylesheet"' }
+						}
+					);
+					$.getScript(
+						'https://cdn.jsdelivr.net/npm/select-woo@1.0.1/dist/js/selectWoo.min.js',
+						function() {
+							populate_markets.select2({
 								allowClear: true,
 								multiple: true,
 								closeOnSelect: false,
@@ -37,50 +94,14 @@
 									id: '',
 									text: 'Select one or more'
 								},
-								width: '100%',
-								ajax: {
-									url: 'https://player.westwoodone.com/stations/stations.ashx',
-									dataType: 'json',
-									processResults: function(data) {
-										var markets = [];
-										data.forEach(function(station) {
-											var market = station.city + ', ' + station.state;
-											if ( ! markets.includes(market)) {
-												markets.push(market);
-											}
-										});
-										markets.sort();
-										
-										var ret = [];
-										original_options.each(function() {
-											ret.push({
-												id: this,
-												text: this
-											});
-										});
-
-										markets.forEach(function(market) {
-											ret.push({
-												id: market.replace(', ', ' - '),
-												text: market
-											});
-											//$select.append(new Option(market, market, false, false));
-										});
-
-										return {
-											results: ret
-										};
-									}
-								}
+								width: '100%'
 							});
-						});
-					}
-				);
-			}
-		}
+						}
+					);
+				}
+			});
 
-		// handle existing forms
-		handleSelects();
+		}
 
 	});
 })(jQuery, window.self);
